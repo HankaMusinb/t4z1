82c401f5b7f823d18a448f926340313599297991
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2022-11-17 23:03:11 +0100

    Dodan test za JSON File

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -2,6 +2,10 @@ package ba.unsa.etf.rpr;
 
 
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Unit test for simple App.
@@ -17,4 +21,13 @@ public class AppTest
     {
         assertTrue( true );
     }
+    @Test
+    public void JSONDatTest() throws IOException, NeodgovarajuciProcesorException {
+        Laptop laptop = new Laptop("hp", "gaming",3499.99,32,1024,0," nesto","nvidia",15.6);
+        LaptopDao proba = new LaptopDaoJSONFile();
+        proba.dodajLaptopUFile(laptop);
+        assertEquals(3499.99, proba.getLaptop(" nesto").getCijena());
+    }
+
+
 }
